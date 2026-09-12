ccb665c77d2885524be9b1a1e1c7ad8f01393b89
abdulrahemanakhlaque0167-dev <abdulraheman.akhlaque0167@gmail.com>
2026-09-12 22:18:47 +0530

    new file added

---
+++ positional.py
@@ -0,0 +1,6 @@
+def display_result(roll,name,marks):
+    print ("roll number=", roll)
+    print ("name=",name)
+    print ("parcent=",sum(marks)/len(marks))
+
+    display_result(roll=52,name="noman",marks=[85,75,65,69,89])
